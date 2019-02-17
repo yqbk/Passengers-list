04800e27856bac46265dad9d76f3d42989541749
yqbk <jksyrek@gmail.com>
2019-02-17 21:56:52 +0100

    Install redux-thunk

--- app/config/store.js
+++ app/config/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 import logger from 'redux-logger'
 
 import reducer from '../reducers'
@@ -8,6 +9,6 @@ if (process.env.NODE_ENV === 'development') {
   middleware.push(logger)
 }
 
-const store = createStore(reducer, applyMiddleware(...middleware))
+const store = createStore(reducer, applyMiddleware(...middleware, thunk))
 
 export default store
